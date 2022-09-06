b6068181f561119533357f15cb70b4448c72fb06
icyjiub <tlneondo@gmail.com>
2022-09-05 21:23:36 -0400

    Initial Commit

---
+++ NewSystemSetup.sh
@@ -0,0 +1,32 @@
+sudo pacman -Syu mpv yay zsh firefox chromium tldr curl base-devel steam lutris flatpak
+
+sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
+lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
+sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
+ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
+lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+
+sudo systemctl enable sshd.service
+sudo systemctl start sshd.service
+
+
+cd /mnt
+sudo mkdir Windows Storage SSD0 SSD1 SSD2 Documents
+
+sudo chown -r icyjiub:icyjiub /mnt/Windows
+sudo chown -r icyjiub:icyjiub /mnt/Storage
+sudo chown -r icyjiub:icyjiub /mnt/SSD0
+sudo chown -r icyjiub:icyjiub /mnt/SSD1
+sudo chown -r icyjiub:icyjiub /mnt/SSD2
+sudo chown -r icyjiub:icyjiub /mnt/Documents
+
+cd /home/icyjiub
+
+rm Documents Videos Pictures Downloads Music
+
+ln -s /mnt/Documents/Media/Documents /home/icyjiub/Documents
+ln -s /mnt/Documents/Media/Videos /home/icyjiub/Videos
+ln -s /mnt/Documents/Media/Pictures /home/icyjiub/Pictures
+ln -s /mnt/Documents/Media/Downloads /home/icyjiub/Downloads
+ln -s /mnt/Storage/Media/Music /home/icyjiub/Music
