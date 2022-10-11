74e71928751ad3375a203e3a96bf66e804bde190
Travis <tlneondo@gmail.com>
2022-10-11 13:18:13 -0400

    added timeshift and btrfs snapshots

--- NewSystemSetup.sh
+++ NewSystemSetup.sh
@@ -1,11 +1,6 @@
-sudo pacman -Syu mpv yay zsh firefox chromium tldr curl base-devel steam lutris flatpak
+sudo pacman -Syu mpv yay zsh firefox chromium tldr curl base-devel steam lutris flatpak linux-zen
 
-sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
-mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
-lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
-sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
-ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
-lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
@@ -21,7 +16,7 @@ sudo chown -r icyjiub:icyjiub /mnt/SSD1
 sudo chown -r icyjiub:icyjiub /mnt/SSD2
 sudo chown -r icyjiub:icyjiub /mnt/Documents
 
-cd /home/icyjiub
+cd ~
 
 rm Documents Videos Pictures Downloads Music
 
@@ -30,3 +25,22 @@ ln -s /mnt/Documents/Media/Videos /home/icyjiub/Videos
 ln -s /mnt/Documents/Media/Pictures /home/icyjiub/Pictures
 ln -s /mnt/Documents/Media/Downloads /home/icyjiub/Downloads
 ln -s /mnt/Storage/Media/Music /home/icyjiub/Music
+
+cd ~
+
+yay -S timeshift
+
+yay -S timeshift-autosnap
+
+yay -S update-grub
+
+sudo pacman -S grub-btrfs
+
+update-grub
+
+sudo systemctl enable grub-btrfs.path
+
+echo "hrtf = true" >> ~/.alsoftrc
+
+yay -S bottles
+
