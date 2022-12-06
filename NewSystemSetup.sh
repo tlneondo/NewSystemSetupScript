3d4b5bea6c28bf0d5bd1b408c0cbd5a38a1b50f2
tlneondo <tlneondo@gmail.com>
2022-12-06 11:32:58 -0500

    added latte

--- NewSystemSetup.sh
+++ NewSystemSetup.sh
@@ -2,24 +2,17 @@
 #update base packages
 sudo pacman -Syu
 
-
-
 #Backup default FSTAB drive mapping
-#sudo cp /etc/fstab /etc/fstab.vanilla
+sudo cp /etc/fstab /etc/fstab.vanilla
 
 ##Add drives to fstab
-#sudo echo "" >> /etc/fstab
-#sudo echo "" >> /etc/fstab
 
-#sudo echo "UUID=90dfd5d0-475b-4d01-b2fe-5eb31aed37fc /mnt/SSD0         btrfs   defaults,discard=async,ssd,noatime,compress=zstd 0 0" >> /etc/fstab
-#sudo echo "UUID=90194eb1-6fe7-4da5-b6ad-36c32112f4ca /mnt/SSD1         btrfs   defaults,discard=async,ssd,noatime,compress=zstd 0 0" >> /etc/fstab
-#sudo echo "UUID=ed43e4f3-64d3-40d9-a929-11354bf8c339 /mnt/SSD2         btrfs   defaults,discard=async,ssd,noatime,compress=zstd 0 0" >> /etc/fstab
-#sudo echo "UUID=ab7ffd5c-1cb6-4e20-9168-3f9d3b26393e /mnt/Documents    btrfs   defaults,compress=zstd 0 0" >> /etc/fstab
-#sudo echo "UUID=c658b823-655d-4e67-8a9a-1c4d87313200 /mnt/Storage      btrfs   defaults,compress=zstd 0 0" >> /etc/fstab
-#sudo echo "UUID=606CE9366CE9081C                     /mnt/Windows      ntfs    defaults 0 0" >> /etc/fstab
-
-#sudo echo "" >> /etc/fstab
-#sudo echo "" >> /etc/fstab
+sudo su -c "echo 'UUID=90dfd5d0-475b-4d01-b2fe-5eb31aed37fc /mnt/SSD0         btrfs   defaults,discard=async,ssd,noatime,compress=zstd 0 0' >> /etc/fstab"
+sudo su -c "echo 'UUID=90194eb1-6fe7-4da5-b6ad-36c32112f4ca /mnt/SSD1         btrfs   defaults,discard=async,ssd,noatime,compress=zstd 0 0' >> /etc/fstab"
+sudo su -c "echo 'UUID=ed43e4f3-64d3-40d9-a929-11354bf8c339 /mnt/SSD2         btrfs   defaults,discard=async,ssd,noatime,compress=zstd 0 0' >> /etc/fstab"
+sudo su -c "echo 'UUID=ab7ffd5c-1cb6-4e20-9168-3f9d3b26393e /mnt/Documents    btrfs   defaults,compress=zstd 0 0' >> /etc/fstab"
+sudo su -c "echo 'UUID=c658b823-655d-4e67-8a9a-1c4d87313200 /mnt/Storage      btrfs   defaults,compress=zstd 0 0' >> /etc/fstab"
+sudo su -c "echo 'UUID=606CE9366CE9081C                     /mnt/Windows      ntfs    defaults 0 0' >> /etc/fstab"
 
 #make mount points
 cd /mnt
@@ -59,7 +52,7 @@ sudo echo "AMD_VULKAN_ICD=RADV" >> /etc/environment
 sudo pacman -Sy --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
 #install prefered packages
-sudo pacman -Sy base-devel mpv yay zsh firefox tldr curl steam lutris flatpak discover linux-zen grub-btrfs qbittorrent yt-dlp corectrl pipewire lib32-pipewire xdg-desktop-portal xdg-desktop-portal-kde qpwgraph lollypop filezilla plasma-wayland-session colord colord-kde
+sudo pacman -Sy base-devel mpv yay zsh firefox tldr curl steam lutris flatpak discover linux-zen grub-btrfs qbittorrent yt-dlp corectrl pipewire lib32-pipewire xdg-desktop-portal xdg-desktop-portal-kde qpwgraph lollypop filezilla plasma-wayland-session colord colord-kde noto-fonts-cjk noto-fonts-emoji gamemode
 
 
 #install AUR packages
@@ -69,6 +62,11 @@ yay -Sy gamescope-git
 yay -Sy sddm-git
 yay -Sy raze
 yay -Sy ckb-next
+yay -Sy ttf-symbola
+yay -Sy mullvad-vpn-bin
+yay -Sy vopono
+yay -Sy latte-dock
+
 
 #install flatpak packages
 flatpak install -y --noninteractive flathub net.davidotek.pupgui2
